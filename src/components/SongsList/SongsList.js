74f7f9170a0a3dd39147cc61f074c6cab84a6a41
crullian <crullian@gmail.com>
2018-02-26 17:03:34 -0500

    Keep track of which track is selected, style tweaks.

--- src/components/SongsList/SongsList.js
+++ src/components/SongsList/SongsList.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import moment from 'moment';
+import React, { Component } from 'react';
+import SongItem from '../../components/SongItem/SongItem';
 
 import './SongsList.css';
 
-const SongsList = ({songs, handleSelectSong}) => (
-  <div className='SongsList__flex-container'>
-    {
-      songs.map((song, index) => {
-        const trackTime  = moment(song.trackTimeMillis).format('mm:ss');
-        return (
-          <div key={index} className='SongsList__song-item-container' onClick={() => handleSelectSong(song)}>
-            <img src={song.artwork} width='160' height='160' alt='track artwork' className='SongsList__song-item-img' />
-            <div className='SongsList__song-item-info'>
-              <h3>{song.trackName}</h3>
-              <p>{song.artistName}</p>
-              <p>{trackTime}</p>
-              <p>{song.releaseYear}</p>
-            </div>
-          </div>
-        )
-      })
+class SongsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemSelected: null
     }
-  </div>
-)
+    this.handleClickSong = this.handleClickSong.bind(this);
+  }
+
+  handleClickSong(song) {
+    this.setState({itemSelected: song.trackId});
+    this.props.handleSelectSong(song);
+  }
+
+  render() {
+    const {songs} = this.props;
+    const {itemSelected} = this.state;
+
+    return (
+      <div className='SongsList__flex-container'>
+        {
+          songs && songs.map((song, index) => {
+            return (
+              <SongItem
+                key={index}
+                song={song}
+                handleSelectSong={() => this.handleClickSong(song, index)}
+                selected={itemSelected === song.trackId}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
 
 export default SongsList
